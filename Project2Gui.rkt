429f6874d4d5c9e78ed7f7dff8413c239e95a0da
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2024-01-12 14:59:50 +0000

    Add files via upload

--- Project2Gui.rkt
+++ Project2Gui.rkt
@@ -1,4 +1,4 @@
-#lang Racket
+#lang racket/gui
 
 ; Input field for destination and current location
 ; Input field for what comfort level they desire
@@ -9,3 +9,12 @@
 ;  Walk - returns the paths to walk to a destination
 ; Return a list of directions between those places that guide the user
 ;  - Use information from OpenStreetMaps if possible
+; A* Algorithm for finding the shortest distance between places
+;  - Explain why this and not other algorithms
+; Use/Create Data structures that capture a the path of the journey
+;  - Create our own
+;    - struct with args (current_location, desired_location, distance)?
+;  - Use vectors
+
+
+(require racket/gui/base)
