3e8dff733ec745f7e5a34496b691a353b2f1d57d
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2024-01-25 15:33:54 +0000

    Add files via upload

--- Project2Gui.rkt
+++ Project2Gui.rkt
@@ -1,37 +1,54 @@
 #lang racket/gui
 (require racket/gui/base)
+(require graph)
 
-; Input field for destination and current location
-; Input field for what comfort level they desire
-; Input field for what transport they will be taking: car, bus, train
-;  Bus - returns bus stops that lead to the destination
-;  Train - returns train stops that lead to the destination
+(define a-graph (list
+                 (list "A" "C" 50)
+                 (list "B" "D" 40)
+                 (list "C" "E" 20) (list "C" "A" 50)
+                 (list "D" "E" 35) (list "D" "B" 40)
+                 (list "E" "F" 100) (list "E" "C" 20) (list "E" "D" 35)
+                 (list "F" "E" 100)
+                 ))
 
-; Return a list of directions between those places that guide the user
-; Use/Create Data structures that capture a the path of the journey
+(define nodes (lambda (graph)
+                (remove-duplicates (flatten graph))))
 
-(struct station (name))
+(define edge (lambda (n1 n2 graph)
+               (flatten (filter
+                  (lambda (y)
+                    (equal? (first (rest y)) n2))
+                  (filter
+                  (lambda (x)
+                    (equal? (first x) n1))
+                  graph)))))
 
-(define start_location "")
-(define end_location "")
+               
+(define children (lambda (n graph)
+                   (map second 
+                   (filter
+                    (lambda (edge)
+                      (equal? (first edge) n))
+                    graph))))
 
-(define get_start_location (lambda (x)
-                             (set! start_location x)
-                             )
+(define leaf (lambda (n graph)
+               (null? (children n graph))))
+
+(define grand-children (lambda (n graph)
+                         (flatten (map (lambda (node) (children node graph)) (children n graph)))))
+
+(define weight (lambda (n1 n2 graph)
+                 (first (reverse (edge n1 n2 graph)))))
+
+;get-path
+; - check what n1 connects to, is that equal to n2
+; - if not, append edge1 to a list, and check what n2 connects to, is that equal to n2?
+;     - ignore the n2 edge that connects back to n1
+; - call recursivly with the new n1 and n2 being the new edge that you are checking
+(define get-path (lambda (n1 n2 graph)
+                   (cond
+                     ([equal? n2 (second (edge n1 n2 graph))] #t)
+                     )
+                   )
   )
 
-(define get_end_location (lambda (x)
-                           (set! end_location x)
-                           )
-  )
-
-(define find_start_station (lambda (query station_list)
-                            (cond
-                              ([equal? query (station-name (list-ref station_list (round (/ (length station_list) 2))))] query)
-                              ([string<? query (station-name (list-ref station_list (round (/ (length station_list) 2))))] query)
-                              ([string>? query (station-name (list-ref station_list (round (/ (length station_list) 2))))] query)
-                              )
-                             )
-  )
-
-(define station_BST (list (station "C") (list (station "B") (list (station "A")) '()) (list (station "D") '() (list (station "E")))))
