172efb13958ede8fb873b7e4a98c393b713183a4
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2024-01-30 12:24:13 +0000

    Add files via upload

--- Project2Gui.rkt
+++ Project2Gui.rkt
@@ -1,33 +1,70 @@
 #lang racket/gui
-
-; Input field for destination and current location
-; Input field for what comfort level they desire
-; Input field for what transport they will be taking: car, bus, train
-;  Car - returns roads to take
-;  Bus - returns bus stops that lead to the destination
-;  Train - returns train stops that lead to the destination
-;  Walk - returns the paths to walk to a destination
-; Return a list of directions between those places that guide the user
-;  - Use information from OpenStreetMaps if possible
-; A* Algorithm for finding the shortest distance between places
-;  - Explain why this and not other algorithms
-; Use/Create Data structures that capture a the path of the journey
-;  - Create our own
-;    - struct with args (current_location, desired_location, distance)?
-;  - Use vectors
-
 (require racket/gui/base)
+(require racket/trace)
+;https://planet.racket-lang.org/package-source/jaymccarthy/dijkstra.plt/1/1/planet-docs/dijkstra/index.html
 
-(define mainFrame (new frame% [label "Main Frame"]))
+(define a-graph (list
+                 (list "A" "C" 50)
+                 (list "B" "D" 40)
+                 (list "C" "E" 20) (list "C" "A" 50)
+                 (list "D" "E" 35) (list "D" "B" 40)
+                 (list "E" "F" 100) (list "E" "C" 20) (list "E" "D" 35)
+                 (list "F" "E" 100)
+                 ))
 
-(define centrePanel (new panel%
-                         [parent mainFrame]
-                         [min-width 200]
-                         [min-height 200]))
+(define nodes (lambda (graph)
+                (remove-duplicates (flatten graph))))
 
-(define testButton (new button%
-                        [label "Test"]
-                        [parent centrePanel]))                   
-                        
-(send mainFrame show #t)
-(send mainFrame resize 640 480)
+(define edge (lambda (n1 n2 graph)
+               (flatten (filter
+                  (lambda (y)
+                    (equal? (first (rest y)) n2))
+                  (filter
+                  (lambda (x)
+                    (equal? (first x) n1))
+                  graph)))))
+
+               
+(define children (lambda (n graph)
+                   (map second 
+                   (filter
+                    (lambda (edge)
+                      (equal? (first edge) n))
+                    graph))))
+
+(define leaf (lambda (n graph)
+               (null? (children n graph))))
+
+;grand-children + (remove n) = grandchildren without source node
+(define grand-children (lambda (n graph)
+                         (flatten (map (lambda (node) (children node graph)) (children n graph)))))
+
+(define weight (lambda (n1 n2 graph)
+                 (first (reverse (edge n1 n2 graph)))))
+
+;example A - D
+
+;get-path
+; is n2 a child or n1?
+; if not, add n1 to a list, call the function again with a child of n1
+; each iteration append n1 to a list to create the list of directions
+
+(define get-path1 (lambda (n1 n2 graph directions)
+                   (cond
+                     ([and (not (not (member n1 directions))) (not (not (member n2 directions)))] directions)
+                     ([equal? #t (not (not (member n2 (children n1 graph))))] (get-path1 (first (children n1 graph)) n2 graph (cons n1 (cons n2 directions))))
+                     )
+                    )
+  )
+
+(define get-path (lambda (n1 n2)
+                   (cond
+                     ([equal? n1 n2] n1)
+                     ([equal? #t (not (not (member n2 (children n1 a-graph))))] (edge n1 n2 a-graph))
+                     (else
+                      (get-path1 n1 n2 a-graph '()))
+                     )
+                   )
+  )
+                       
+(trace get-path1)
