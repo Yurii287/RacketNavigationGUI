42770886e5a571f5a039b9a6b44efb4bf8533497
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2024-01-30 12:23:43 +0000

    Add files via upload

--- Project2Gui.rkt
+++ Project2Gui.rkt
@@ -1,54 +1,33 @@
 #lang racket/gui
+
+; Input field for destination and current location
+; Input field for what comfort level they desire
+; Input field for what transport they will be taking: car, bus, train
+;  Car - returns roads to take
+;  Bus - returns bus stops that lead to the destination
+;  Train - returns train stops that lead to the destination
+;  Walk - returns the paths to walk to a destination
+; Return a list of directions between those places that guide the user
+;  - Use information from OpenStreetMaps if possible
+; A* Algorithm for finding the shortest distance between places
+;  - Explain why this and not other algorithms
+; Use/Create Data structures that capture a the path of the journey
+;  - Create our own
+;    - struct with args (current_location, desired_location, distance)?
+;  - Use vectors
+
 (require racket/gui/base)
-(require graph)
 
-(define a-graph (list
-                 (list "A" "C" 50)
-                 (list "B" "D" 40)
-                 (list "C" "E" 20) (list "C" "A" 50)
-                 (list "D" "E" 35) (list "D" "B" 40)
-                 (list "E" "F" 100) (list "E" "C" 20) (list "E" "D" 35)
-                 (list "F" "E" 100)
-                 ))
+(define mainFrame (new frame% [label "Main Frame"]))
 
-(define nodes (lambda (graph)
-                (remove-duplicates (flatten graph))))
+(define centrePanel (new panel%
+                         [parent mainFrame]
+                         [min-width 200]
+                         [min-height 200]))
 
-(define edge (lambda (n1 n2 graph)
-               (flatten (filter
-                  (lambda (y)
-                    (equal? (first (rest y)) n2))
-                  (filter
-                  (lambda (x)
-                    (equal? (first x) n1))
-                  graph)))))
-
-               
-(define children (lambda (n graph)
-                   (map second 
-                   (filter
-                    (lambda (edge)
-                      (equal? (first edge) n))
-                    graph))))
-
-(define leaf (lambda (n graph)
-               (null? (children n graph))))
-
-(define grand-children (lambda (n graph)
-                         (flatten (map (lambda (node) (children node graph)) (children n graph)))))
-
-(define weight (lambda (n1 n2 graph)
-                 (first (reverse (edge n1 n2 graph)))))
-
-;get-path
-; - check what n1 connects to, is that equal to n2
-; - if not, append edge1 to a list, and check what n2 connects to, is that equal to n2?
-;     - ignore the n2 edge that connects back to n1
-; - call recursivly with the new n1 and n2 being the new edge that you are checking
-(define get-path (lambda (n1 n2 graph)
-                   (cond
-                     ([equal? n2 (second (edge n1 n2 graph))] #t)
-                     )
-                   )
-  )
-
+(define testButton (new button%
+                        [label "Test"]
+                        [parent centrePanel]))                   
+                        
+(send mainFrame show #t)
+(send mainFrame resize 640 480)
