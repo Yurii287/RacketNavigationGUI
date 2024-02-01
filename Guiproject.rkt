77d12acd0fcd2c5727012d1a1cabd0aa7a7d107a
SagoSagoa <156342442+SagoSagoa@users.noreply.github.com>
2024-02-01 14:55:41 +0000

    Update Guiproject.rkt

--- Guiproject.rkt
+++ Guiproject.rkt
@@ -40,6 +40,13 @@
 (define weight (lambda (n1 n2 graph)
                  (first (reverse (edge n1 n2 graph)))))
 
+;example A - D
+
+;get-path
+; is n2 a child or n1?
+; if not, add n1 to a list, call the function sagain with a child of n1
+; each iteration append n1 to a list to create the list of directions
+
 (define get-path1 (lambda (n1 n2 graph directions)
                    (cond
                      ([and (not (not (member n1 directions))) (not (not (member n2 directions)))] (reverse directions))
@@ -63,7 +70,7 @@
                        
 
 
-
+;Train stations
 (define train_lines(list "Morden"
 "South Wimbledon"
 "Colliers Wood"
@@ -97,7 +104,37 @@
 "Colindale"
 "Burnt Oak"
 "Edgware"))
-
+(define train_lines3(list "Brixton"
+"Stockwell" 
+"Vauxhall" 
+"Pimlico" 
+"Victoria"   
+"Green Park" 
+"Oxford Circus" 
+"Warren Street" 
+"Euston" 
+"King's Cross St. Pancras" 
+"Highbury & Islington" 
+"Finsbury Park" 
+"Seven Sisters" 
+"Tottenham Hale" 
+"Blackhorse Road" 
+"Walthamstow Central" ))
+(define train_lines2(list "Northern Line"
+"Bakerloo Line"
+"Central Line"
+"Circle Line"
+"District Line"
+"Hammersmith & City Line"
+"Jubilee Line"
+"Metropolitan Line Tube"
+"Northern Line"
+"Piccadilly Line"
+"Victoria Line"
+"Waterloo & City Line"))
+;S1
+(define font_app(make-font #:size 12 #:family 'decorative
+                             #:weight 'normal))
 (define journey(list "Hendon Central" "Brent Cross"))
  ;(send my-box get-string (send my-box get-selection))
 (define choiceschoices(list "Field" "field" "field"))
@@ -141,7 +178,7 @@
                 (send dc draw-text "Transport app" 0 0))]
              [vert-margin 0]	 
    	 	[horiz-margin 0])
-
+;S2
 (new canvas% [parent frame2]
      [min-height 30]
      [min-width 100]
@@ -163,7 +200,49 @@
                 (send dc draw-text "Transport app" 0 0))]
              [vert-margin 0]	 
    	 	[horiz-margin 0])
-
+(new canvas% [parent frame2]
+     [min-height 30]
+     [min-width 100]
+     	[stretchable-width #t]	 
+   	 	[stretchable-height #f]
+             [paint-callback
+              (lambda (canvas dc)
+                (send dc set-background (make-object color% 193 200 210))
+            (send dc clear)
+                
+                (send dc set-scale 1 1)
+                (send dc set-pen "white" 1 'transparent)
+                (send dc set-brush "0, 25, 168" 'solid)
+                (send dc set-font (make-font #:size 14 #:family 'decorative
+                             #:weight 'bold))
+                ;(send dc draw-rectangle 0 0 100 30)
+                
+                (send dc set-text-foreground (make-object color% 255 255 255))
+                (send dc draw-text "Here's your journey info:" 0 0))]
+             [vert-margin 0]	 
+   	 	[horiz-margin 0])
+(new canvas% [parent frame]
+     [min-height 30]
+     [min-width 100]
+     	[stretchable-width #t]	 
+   	 	[stretchable-height #f]
+             [paint-callback
+              (lambda (canvas dc)
+                (send dc set-background (make-object color% 193 200 210))
+            (send dc clear)
+                
+                (send dc set-scale 1 1)
+                (send dc set-pen "white" 1 'transparent)
+                (send dc set-brush "0, 25, 168" 'solid)
+                (send dc set-font (make-font #:size 14 #:family 'decorative
+                             #:weight 'bold))
+                ;(send dc draw-rectangle 0 0 100 30)
+                
+                (send dc set-text-foreground (make-object color% 255 255 255))
+                (send dc draw-text "Enter journey info:" 0 0))]
+             [vert-margin 0]	 
+   	 	[horiz-margin 0])
+;S3
 (define panel1(new panel%
                    [parent frame]
                    [alignment '(center bottom)]
@@ -178,36 +257,46 @@
 (define panel3(new panel%
                    [parent frame2]
                    [alignment '(left bottom)]
+                   [min-height 300]
                    	[style (list 'border )]
                    ))
-(define panel4(new panel%
+(define panel4(new horizontal-panel%
                    [parent frame2]
-                   [alignment '(center top)]
+                   [alignment '(center bottom)]
                    	[style (list 'border )]
                    ))
 
 
-                       
+;S6                       
 (define starting_location
   (new choice% [parent panel1]
-       
-       [label "Some choices"]
-       [choices train_lines]))
+       [vert-margin 40]
+       [label "Starting Location:"]
+       [choices train_lines]
+       [font font_app]))
+(define line
+  (new choice% [parent panel1]
+       [vert-margin 10]
+       [label  "                          "]
+       [choices train_lines2]
+       [font font_app]
+       [callback (λ (c e) (cond ((equal? (send line get-string (send line get-selection)) "Victoria Line") (send starting_location clear)(for ([i train_lines3]) (send starting_location append i)))))]))
 (define destination
   (new choice% [parent panel2]
-       
-       [label "Some choices"]
-       [choices train_lines]))
-
+       [vert-margin 5]
+       [label "Destination:        "]
+       [choices train_lines]
+       [font font_app]))
+;S4
 (define button (new button%
                     (parent panel2)
-                    (label "Button")
+                    (label "Go")
                     (vert-margin 50)
                     	(min-width 50)	 
    	 	(min-height 50)
                 (callback (lambda (button event)
-                        (cond
-                         
+                        (cond ((equal? (send starting_location get-string (send starting_location get-selection)) (send destination get-string (send destination get-selection))) (send frame3 show #t))
+                         (else (send list-box set (get-path (send starting_location get-string (send starting_location get-selection)) (send destination get-string (send destination get-selection))))(send frame2 show #t) (send frame show #f))
                           )))))
 (define message (new message%
                      (parent panel2)
@@ -221,29 +310,34 @@ Aenean semper tristique ipsum, nec. ")
                 	[auto-resize #f]))
 (define button2 (new button%
                     (parent panel4)
-                    (label "Button")
-                    (vert-margin 100)
-                    	[min-width 50]	 
-   	 	(min-height 50)
+                    (label "Go back")
+                    
+                    	[min-width 34]	 
+   	 	(min-height 34)
                 (callback (lambda (button event)
                         (send frame show #t) (send frame2 show #f)))))
 (define button3 (new button%
                     (parent panel4)
                     (label "filter")
-                    (vert-margin 50)
+                    
                     	[min-width 50]	 
    	 	(min-height 50)
                 (callback (lambda (button event)
-                        (send frame show #t) (send frame2 show #f)))))
+                        (send frame show #f) (send frame2 show #t)))))
 (define something(list "hello" "world"))
+;s5
 (define list-box (new list-box%
                       (label "")
                       (parent panel3)
                       (choices something  )
+                      (font font_app)
                       (style (list 'single
                                    'variable-columns 'column-headers))
-                      (columns (list "Train stations"))))
-
+                      (columns (list "Train station"))))
+(send list-box set-column-width	 	0	 	 	 	 
+ 	 	150	 	 	 	 
+ 	 	150	 	 	 	 
+ 	 	150)
 ;(send list-box append "choices" [0])
 (send frame show #t)
 
