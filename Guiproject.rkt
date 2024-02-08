ff48b8aed947b406f3e96351c14faca5103b2948
SagoSagoa <156342442+SagoSagoa@users.noreply.github.com>
2024-02-08 14:05:13 +0000

    Update Guiproject.rkt

--- Guiproject.rkt
+++ Guiproject.rkt
@@ -20,10 +20,11 @@
   ("Stockwell" #f) ("Oval" #f) ("Battersea Power Station" #f) ("Waterloo" #f) ("Embankment" #f) ("Charing Cross" #f) ("Leicester Square" #f) ("Goodge Street" #f) ("Warren Street" #f) ("Mornington Crescent" #f) ("Chalk Farm" #f)
   ("Belsize Park" #f) ("Hampstead" #f) ("Brent Cross" #f) ("Colindale" #f) ("Burnt Oak" #f) ("Battersea" #t) ("Borough" #t) ("Camden Town" #t) ("Edgware" #t) ("Elephant & Castle" #t) ("Euston" #t)
   ("Finchley Central" #t) ("Golders Green" #t) ( "Hendon Central" #t) ("High Barnet" #t) ("Kennington" #t) ("King's Cross St Pancras" #t) ("London Bridge" #t) ("Mill Hill East" #t) ("Moorgate" #t) ("Morden" #t)
-  ("Nine Elms" #t) ("StockWell" #t) ("Tottenham Court Road" #t) ("West Finchley" #t) ("Woodside Park" #t))]
+  ("Nine Elms" #t) ("StockWell" #t) ("Tottenham Court Road" #t) ("West Finchley" #t) ("Woodside Park" #t) ("Old Street" #f) ("Bank" #f) ("Angel" #f) ("Kentish Town" #f) ("Archway" #f) ("Tufnell Park" #f) ("East Finchley" #f) ("Highgate" #f))]
                           [train_connections (list
+                                           ;Morden Branch
                                            (list "Morden" "South Wimbledon")
-                                           (list "South Wimbledon" "Colliers Wood") (list "South Wimbledon" "Mordern")
+                                           (list "South Wimbledon" "Colliers Wood") (list "South Wimbledon" "Morden")
                                            (list "Colliers Wood" "Tooting Broadway") (list "Colliers Wood" "South Wimbledon")
                                            (list "Tooting Broadway" "Tooting Bec") (list "Tooting Broadway" "Colliers Wood")
                                            (list "Tooting Bec" "Balham") (list "Tooting Bec" "Tooting Broadway")
@@ -34,7 +35,8 @@
                                            (list "Stockwell" "Oval") (list "Stockwell" "Clapham North")
                                            (list "Oval" "Kennington") (list "Oval" "Stockwell")
                                            (list "Kennington" "Waterloo") (list "Kennington" "Elephant & Castle") (list "Kennington" "Oval")
-
+                                          
+                                           ;Embankment Branch
                                            (list "Waterloo" "Embankment") (list "Waterloo" "Kennington")
                                            (list "Embankment" "Charing Cross") (list "Embankment" "Waterloo")
                                            (list "Charing Cross" "Leicester Square") (list "Charing Cross" "Embankment")
@@ -42,15 +44,47 @@
                                            (list "Tottenham Court Road" "Goodge Street") (list "Tottenham Court Road" "Leicester Square")
                                            (list "Goodge Street" "Warren Street") (list "Goodge Street" "Tottenham Court Road")
                                            (list "Warren Street" "Euston") (list "Warren Street" "Goodge Street")
+                                           (list "Euston" "Warren Street")
 
+                                           ;Bank Branch
                                            (list "Elephant & Castle" "Borough") (list "Elephant & Castle" "Kennington")
                                            (list "Borough" "London Bridge") (list "Borough" "Elephant & Castle")
                                            (list "London Bridge" "Bank") (list "London Bridge" "Borough")
                                            (list "Bank" "Moorgate") (list "Bank" "London Bridge")
                                            (list "Moorgate" "Old Street") (list "Moorgate" "Bank")
                                            (list "Old Street" "Angel") (list "Old Street" "Moorgate")
-                                           (list "Angel" "King's Cross St Pancras") (list "Angel" "Old Street")
+                                           (list "Angel" "King's Cross St Pancras") (list "Angel" "Old Street") 
                                            (list "King's Cross St Pancras" "Euston") (list "King's Cross St Pancras" "Angel")
+                                           
+                                           ;The weird part 
+                                           (list "Euston" "Mornington Crescent") (list "Euston" "Camden Town")
+                                           (list "Mornington Crescent" "Euston") (list "Camden Town" "Euston")
+                                           (list "Camden Town" "Mornington Crescent")  (list "Camden Town" "Chalk farm")
+                                           (list "Mornington Crescent" "Camden Town")
+
+                                           ;Edgware Branch
+                                           (list "Chalk Farm" "Camden Town")(list "Camden Town" "Chalk Farm")
+                                           (list "Chalk Farm" "Belsize Park")(list "Belsize Park" "Chalk Farm")
+                                           (list "Belsize Park" "Hampstead")(list "Hampstead" "Belsize Park")
+                                           (list "Hampstead" "Golders Green")(list "Golders Green" "Hampstead")
+                                           (list "Golders Green" "Brent Cross") (list "Brent Cross" "Golders Green")
+                                           (list "Golders Green" "Hendon Central")(list "Hendon Central" "Golders Green")
+                                           (list "Hendon Central" "Colindale") (list "Colindale" "Hendon Central")
+                                           (list "Colindale" "Burnt Oak")(list "Burnt Oak" "Colindale")
+                                           (list "Burnt Oak" "Edgware")(list "Edgware" "Burnt Oak")
+                                           
+                                           ;High Barnet Branch
+                                           (list "Kentish Town" "Camden Town")(list "Camden Town" "Kentish Town")
+                                           (list "Kentish Town" "Tufnell Park")
+                                           (list "Tufnell Park" "Kentish Town")
+                                           (list "Tufnell Park" "Archway") (list "Archway" "Tufnell Park")
+                                           (list "Archway" "Highgate") (list "Highgate" "Archway")
+                                           (list "Highgate" "East Finchley") (list "East Finchley" "Highgate")
+                                           (list "East Finchley" "Finchley Central") (list "Finchley Central" "East Finchley")
+                                           (list "Finchley Central" "West Finchley") (list "West Finchley" "Finchley Central")
+                                           (list "West Finchley" "Woodside Park") (list "Woodside Park" "West Finchley")
+                                           (list "Woodside Park" "Totteridge & Whetstone") (list "Totteridge & Whetstone" "Woodside Park")
+                                           (list "Totteridge & Whetstone" "High Barnet")(list "High Barnet" "Totteridge & Whetstone")
                                            )]
                           )
   )
@@ -60,80 +94,7 @@
                           ("Highbury & Islington" #f)  ("Finsbury Park" #t)  ("Seven Sisters" #f)  ("Tottenham Hale" #t)
                           ("Blackhorse Road" #f)  ("Walthamstow Central" #f) )]))
 (define lines(list northern_line victoria_line))
-
-(require racket/gui/base)
-
-(define a-graph (list
-                 (list "A" "C" 50)
-                 (list "B" "D" 40)
-                 (list "C" "E" 20) (list "C" "A" 50)
-                 (list "D" "E" 35) (list "D" "B" 40)
-                 (list "E" "F" 100) (list "E" "C" 20) (list "E" "D" 35)
-                 (list "F" "E" 100) (list "F" "G" 20)
-                 (list "G" "F" 20)
-                 ))
-
-(define nodes (lambda (graph)
-                (remove-duplicates (flatten graph))))
-
-(define edge (lambda (n1 n2 graph)
-               (flatten (filter
-                  (lambda (y)
-                    (equal? (first (rest y)) n2))
-                  (filter
-                  (lambda (x)
-                    (equal? (first x) n1))
-                  graph)))))
-
-               
-(define children (lambda (n graph)
-                   (map second 
-                   (filter
-                    (lambda (edge)
-                      (equal? (first edge) n))
-                    graph))))
-
-(define leaf (lambda (n graph)
-               (null? (children n graph))))
-
-(define grand-children (lambda (n graph)
-                         (remove n (flatten (map (lambda (node) (children node graph)) (children n graph))))))
-
-(define weight (lambda (n1 n2 graph)
-                 (first (reverse (edge n1 n2 graph)))))
-
-(define branch (lambda (n graph branch-nodes)
-                 (cond
-                   ([equal? n "Kennington"] branch-nodes)
-                   ([not (equal? n "Kennington")] (branch (first (children n graph)) graph (cons n branch-nodes)))
-                   )
-                 )
-  )
- 
-(define get-path1 (lambda (n1 n2 graph directions)
-                   (cond
-                     ([and (not (not (member n1 directions))) (not (not (member n2 directions)))] (remove-duplicates (reverse directions)))
-                     ([not (not (member n2 (children n1 graph)))] (get-path1 n1 n2 graph (cons n2 (cons n1 directions))))
-                     ([and (equal? n1 "Kennington") (not (not (member n2 (branch "A" graph '()))))] (get-path1 (second (children "E" graph)) n2 graph (cons n1 directions)))
-                     ([and (equal? n1 "Kennington") (not (not (member n2 (branch "B" graph '()))))] (get-path1 (third (children "E" graph)) n2 graph (cons n1 directions))) 
-                     ([and (equal? n1 "Kennington") (not (not (member n2 (branch "G" graph '()))))] (get-path1 (first (children "E" graph)) n2 graph (cons n1 directions))) 
-                     ([not (not (member (first (children n1 graph)) directions))] (get-path1 (first (children n1 graph)) n2 graph (cons n1 directions)))
-                     (else (get-path1 (first (children n1 graph)) n2 graph (cons n1 directions)))
-                     )
-                    )
-  )
-
-(define get-path (lambda (n1 n2)
-                   (cond
-                     ([equal? n1 n2] "You are at your destination")
-                     ([equal? #t (not (not (member n2 (children n1 a-graph))))] (edge n1 n2 a-graph))
-                     (else
-                      (get-path1 n1 n2 a-graph '()))
-                     )
-                   )
-  )
-                       
-                       
+            
 ;Train stations
 ;Northern line
 (define train_lines(send northern_line train_stations_get))
@@ -271,33 +232,28 @@
 (define panel1(new panel%
                    [parent frame]
                    [alignment '(center bottom)]
-                   [min-height 300]
-                   	[style (list 'border )]
-                   ))
+                   [min-height 300]))
+
 (define panel2(new panel%
                    [parent frame]
-                   [alignment '(center top)]
-                   	[style (list 'border )]
-                   ))
+                   [alignment '(center top)]))
+
 (define panel3(new panel%
                    [parent frame2]
                    [alignment '(left bottom)]
-                   [min-height 300]
-                   	[style (list 'border )]
-                   ))
+                   [min-height 400]))
+
 (define panel4(new horizontal-panel%
                    [parent frame2]
                    [alignment '(center bottom)]
-                   	[style (list 'border )]
-                   ))
-
+                   [stretchable-height #f]))
 
 ;S6                       
 (define starting_location
   (new choice% [parent panel1]
        [vert-margin 40]
        [label "Starting Location:"]
-       [choices train_lines]
+       [choices (sort train_lines string<?)]
        [font font_app]))
 
 (define line
